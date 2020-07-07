1bae05f8d718d4a77494cc56e817859f1bb58c72
Murtuza Mithaiwala <murtuza5689@gmail.com>
2020-07-07 08:31:15 +0530

    New app, which I hope should work

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AWS deployed!
+          Finally, I have the react app on AWS!
         </p>
         <a
           className="App-link"
